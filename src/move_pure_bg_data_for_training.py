eb2ab2b28c6612b6befde6258f4056bb8e803b3e
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2025-11-30 17:02:02 +0530

    [move_pure_bg_data_for_training] added script to be used for moving pure background data to respective directories for training

---
+++ src/move_pure_bg_data_for_training.py
@@ -0,0 +1,89 @@
+import logging
+import argparse
+
+
+from utils.move_data_for_training import move_bg_images_n_labels
+
+
+def parse_args() -> argparse.Namespace:
+    arg_parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    arg_parser.add_argument(
+        "--dir-pure-bg-images",
+        default=None,
+        type=str,
+        help="full path to the directory where pure background images are present",
+    )
+    arg_parser.add_argument(
+        "--dir-pure-bg-labels",
+        default=None,
+        type=str,
+        help="full path to the directory where pure labels are present",
+    )
+    arg_parser.add_argument(
+        "--dir-train-images",
+        default=None,
+        type=str,
+        help="full path to the directory where train images are to be moved",
+    )
+    arg_parser.add_argument(
+        "--dir-train-labels",
+        default=None,
+        type=str,
+        help="full path to the directory where train labels are to be moved",
+    )
+    arg_parser.add_argument(
+        "--dir-test-images",
+        default=None,
+        type=str,
+        help="full path to the directory where test images are to be moved",
+    )
+    arg_parser.add_argument(
+        "--dir-test-labels",
+        default=None,
+        type=str,
+        help="full path to the directory where test labels are to be moved",
+    )
+    arg_parser.add_argument(
+        "--test-size",
+        default=0.35,
+        type=int,
+        help="test set size",
+    )
+    arg_parser.add_argument(
+        "--out-log-file",
+        default="move_data_for_training.log",
+        type=str,
+        help="the log file where all the logs are recorded",
+    )
+
+    ARGS, _ = arg_parser.parse_known_args()
+    return ARGS
+
+
+def main() -> None:
+    ARGS = parse_args()
+
+    logging.basicConfig(
+        filename=ARGS.out_log_file,
+        filemode="a",
+        format="%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )
+
+    move_bg_images_n_labels(
+        ARGS.dir_pure_bg_images,
+        ARGS.dir_pure_bg_labels,
+        ARGS.dir_train_images,
+        ARGS.dir_train_labels,
+        ARGS.dir_test_images,
+        ARGS.dir_test_labels,
+        test_size=ARGS.test_size,
+    )
+    return
+
+
+if __name__ == "__main__":
+    main()
