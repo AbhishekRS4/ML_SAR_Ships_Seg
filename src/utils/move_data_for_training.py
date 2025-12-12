0a28f228ecf5f190cbdd516b0bd0e07b5331d160
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2025-12-12 18:36:01 +0530

    [utils/move_data_for_training] minor fix to create target directories if they do not exist

--- src/utils/move_data_for_training.py
+++ src/utils/move_data_for_training.py
@@ -28,6 +28,12 @@ def move_bg_images_n_labels(
     path_dir_test_images = Path(dir_test_images)
     path_dir_test_labels = Path(dir_test_labels)
 
+    if not path_dir_train_images.is_dir():
+        path_dir_train_images.mkdir()
+
+    if not path_dir_test_images.is_dir():
+        path_dir_test_images.mkdir()
+
     list_images = sorted(
         [f.name for f in path_dir_pure_bg_images.glob("*png") if f.is_file()]
     )
