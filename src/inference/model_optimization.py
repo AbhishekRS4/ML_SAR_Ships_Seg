14e2c76df00cd23790a10507e3897910834316f3
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2025-12-13 00:24:33 +0530

    [inference/model_optimization] added some logging statement

--- src/inference/model_optimization.py
+++ src/inference/model_optimization.py
@@ -1,6 +1,7 @@
 import os
 import time
 import torch
+import logging
 
 from pathlib import Path
 
@@ -12,6 +13,10 @@ def optimize_model_with_aot_inductor(
     function for optimizing the model with AOT inductor for inference and saving the model file
     """
     path_file_model_ckpt = Path(file_model_ckpt)
+    if not path_file_model_ckpt.is_file():
+        logging.error(f"file not found: {path_file_model_ckpt}")
+
+    logging.info(f"loading model ckpt file from: {path_file_model_ckpt}")
 
     path_file_aot_inductor_model = (
         path_file_model_ckpt.parent
@@ -38,11 +43,14 @@ def optimize_model_with_aot_inductor(
             example_inputs,
         )
 
-        inductor_model_path = torch._inductor.aoti_compile_and_package(
+        _ = torch._inductor.aoti_compile_and_package(
             exported_program,
             package_path=path_file_aot_inductor_model,
             inductor_configs=inductor_configs,
         )
         # do not use pathlib PosixPaths, the AOT inductor optimized model saving will fail
         # also, the file extension needs to be .pt2
+    logging.info(
+        f"optimized model with AOT Inductor is saved to: {path_file_aot_inductor_model}"
+    )
     return
