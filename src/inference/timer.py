f2ffc29f8527f0d92ee2749245a81b13d49f1795
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2025-12-27 22:06:51 +0530

    [inference/timer] added a function for the timing any function run on a GPU

--- src/inference/timer.py
+++ src/inference/timer.py
@@ -8,6 +8,21 @@ from typing import Tuple, Callable
 def gpu_timer(
     fn: Callable[[torch.Tensor], Tuple[torch.Tensor, float]],
 ) -> Tuple[torch.Tensor, float]:
+    """
+    Docstring for gpu_timer
+
+    ---------
+    Arguments
+    ---------
+    fn: Callable[[torch.Tensor], Tuple[torch.Tensor, float]]
+        a function
+
+    -------
+    Returns
+    -------
+    result, duration: Tuple[torch.Tensor, float]
+        a tuple of the result of the function and the duration
+    """
     if torch.cuda.is_available():
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
