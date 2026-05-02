f387b564738c12d3e760a6b008b59e978d9b5c4b
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2026-05-02 21:27:54 +0530

    [src/optimize_model_with_aot_inductor] added script used to optimize mode with AOT inductor

--- src/optimize_model_with_aot_inductor.py
+++ src/optimize_model_with_aot_inductor.py
@@ -1,7 +1,7 @@
 import logging
 import argparse
 
-from model_optimization.model_optimization import optimize_model_with_aot_inductor
+from model_optimization.aot_inductor import optimize_model_with_aot_inductor
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -17,7 +17,12 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument(
         "--model-name",
         default="convnext_v2_tiny_deeplab_v3+",
-        choices=["convnext_v2_tiny_deeplab_v3+", "convnext_v2_base_deeplab_v3+"],
+        choices=[
+            "convnext_v2_tiny_deeplab_v3+",
+            "convnext_v2_base_deeplab_v3+",
+            "resnet34_unet",
+            "psa_resnet34_unet",
+        ],
         type=str,
         help="the model that needs to be trained",
     )
