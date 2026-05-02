380a76d7e4714e4fcf2de848ef6d1a2bf191ea27
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2026-05-02 21:15:40 +0530

    [src/inference/infer_aot] added submodule for inference using AOT inductor

---
+++ src/inference/infer_aot.py
@@ -0,0 +1,83 @@
+import os
+import time
+import torch
+import logging
+import numpy as np
+
+import torch.nn.functional as F
+
+
+from typing import List
+from pathlib import Path, PosixPath
+from skimage.io import imsave, imread
+
+
+from inference.timer import gpu_timer
+from data_handler.data_processing import preprocess_image_tensor
+
+
+def aot_inductor_inference_pipeline(
+    dir_test_images: str,
+    dir_predictions: str,
+    file_model_ckpt: str,
+    which_gpu: str = "0",
+) -> None:
+    """
+    function for inference wit AOT inductor optimized model ckpt
+
+    ---------
+    Arguments
+    ---------
+    dir_test_images: str
+        full path to the directory containing the test images
+    dir_predictions: str
+        full path to the directory where the predicted labels need to be saved
+    file_model_ckpt: str
+        full path to the model checkpoint with .pt2 file format
+    which_gpu: str
+        GPU number on which the model inference needs to be run (default: "0")
+    """
+    os.environ["CUDA_VISIBLE_DEVICES"] = which_gpu
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    path_dir_test_images = Path(dir_test_images)
+    path_dir_predictions = Path(dir_predictions)
+
+    if not path_dir_predictions.is_dir():
+        path_dir_predictions.mkdir()
+
+    aot_compiled_model = torch._inductor.aoti_load_package(file_model_ckpt)
+    # model ckpt file path must be a string, does not accept PosixPath
+
+    list_test_images = sorted(
+        [f for f in path_dir_test_images.glob("*png") if f.is_file()]
+    )
+
+    with torch.inference_mode():
+        for file_test_img in list_test_images:
+            file_name_pred = file_test_img.name
+            test_img_arr = imread(file_test_img)
+
+            test_img_tensor = torch.from_numpy(test_img_arr[:, :, 0])
+            test_img_tensor = test_img_tensor.to(device, dtype=torch.float32)
+            test_img_tensor = torch.unsqueeze(
+                torch.unsqueeze(test_img_tensor, dim=0), dim=0
+            )
+            test_img_tensor = preprocess_image_tensor(test_img_tensor)
+
+            pred_logits, time_taken = gpu_timer(
+                lambda: aot_compiled_model(test_img_tensor)
+            )
+            pred_probs = F.softmax(pred_logits, dim=1)
+            pred_label = torch.argmax(pred_probs, dim=1)
+            pred_label = pred_label.clone().detach().cpu().numpy()
+            logging.info(
+                f"Time taken for inference with the optimized AOT Inductor model is {time_taken:.2f} ms"
+            )
+
+            pred_label = np.squeeze(pred_label).astype(np.uint8)
+            imsave(path_dir_predictions / file_name_pred, pred_label)
+    return
