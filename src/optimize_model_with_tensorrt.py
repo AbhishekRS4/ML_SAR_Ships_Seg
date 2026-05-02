f9e4734de153f830f7138b43a4eac4c9fb48e0da
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2026-05-02 21:26:57 +0530

    [src/optimize_model_with_tensorrt] added script used to optimize model with tensorrt

---
+++ src/optimize_model_with_tensorrt.py
@@ -0,0 +1,96 @@
+import logging
+import argparse
+
+from model_optimization.tensorrt import optimize_model_with_tensorrt
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--file-model-ckpt",
+        default=None,
+        type=str,
+        help="full path to the checkpoint file to load for finetuning the model",
+    )
+    parser.add_argument(
+        "--model-name",
+        default="convnext_v2_tiny_deeplab_v3+",
+        choices=[
+            "convnext_v2_tiny_deeplab_v3+",
+            "convnext_v2_base_deeplab_v3+",
+            "resnet34_unet",
+            "psa_resnet34_unet",
+        ],
+        type=str,
+        help="the model that needs to be trained",
+    )
+    parser.add_argument(
+        "--image-height",
+        default=800,
+        type=int,
+        help="image height for AOT inductor sample input",
+    )
+    parser.add_argument(
+        "--image-width",
+        default=800,
+        type=int,
+        help="image width for AOT inductor sample input",
+    )
+    parser.add_argument(
+        "--precision",
+        default="fp32",
+        type=str,
+        choices=["mixed", "fp32"],
+        help="precision to be enabled for optimizing the model and running the inference",
+    )
+    parser.add_argument(
+        "--model-compile-mode",
+        default="reduce-overhead",
+        type=str,
+        choices=["normal", "reduce-overhead", "max-autotune", "uncompiled"],
+        help="indicates the model compile option to use to reduce overhead during training",
+    )
+    parser.add_argument(
+        "--which-gpu",
+        default="0",
+        type=str,
+        help="which GPU needs to be used for training",
+    )
+    parser.add_argument(
+        "--out-log-file",
+        default="optimize_model_with_tensorrt.log",
+        type=str,
+        help="the log file where all the logs are recorded",
+    )
+
+    ARGS, unparsed = parser.parse_known_args()
+    return ARGS
+
+
+def main() -> None:
+    ARGS = parse_arguments()
+
+    logging.basicConfig(
+        filename=ARGS.out_log_file,
+        filemode="a",
+        format="%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )
+
+    optimize_model_with_tensorrt(
+        ARGS.file_model_ckpt,
+        ARGS.model_name,
+        ARGS.image_height,
+        ARGS.image_width,
+        precision=ARGS.precision,
+        model_compile_mode=ARGS.model_compile_mode,
+        which_gpu=ARGS.which_gpu,
+    )
+    return
+
+
+if __name__ == "__main__":
+    main()
