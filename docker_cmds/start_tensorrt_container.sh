9b3fd9bfa7f239caa30dbbbdac82c1c43891a6f6
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2026-05-30 22:48:37 +0530

    [docker_cmds/start_tensorrt_container.sh] minor update

--- docker_cmds/start_tensorrt_container.sh
+++ docker_cmds/start_tensorrt_container.sh
@@ -1,1 +1,1 @@
-docker run --mount type=bind,source=/home/abhishek/code_repos/ML_SAR_Ships,target=/workspace/temp_scripts/ --gpus all -it --rm nvcr.io/nvidia/pytorch:25.11-py3
+docker run --mount type=bind,source=/home/abhishek/code_repos/ML_SAR_Ships,target=/workspace/ --gpus all -it --rm nvcr.io/nvidia/pytorch:25.11-py3
